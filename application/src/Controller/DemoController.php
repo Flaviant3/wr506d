b376b71bffa357188ce3572f1373abc828c69d94
Github MMI3 <github@mmi-troyes.fr>
2024-09-11 13:34:16 +0000

    feat slugify but bugged

--- application/src/Controller/DemoController.php
+++ application/src/Controller/DemoController.php
@@ -5,17 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Service\Slugify;
 
 class DemoController extends AbstractController
 {
     #[Route('/demo', name: 'app_demo')]
-    public function index(): Response
+    public function index(Slugify $slugify): Response
     {
-        // Obtenir la date actuelle
-        $date = new \DateTime('now', new \DateTimeZone('Europe/Paris'));
+        $phrase = 'éoh salamé, ça farte ?';
+        $slug = $slugify->slugify($phrase);
 
         return $this->render('demo/index.html.twig', [
-            'date' => $date,
+            'controller_name' => 'DemoController',
+            'date' => new \DateTime(),
+            'slug' => $slug, // On passe le slug à la vue
         ]);
     }
 }
