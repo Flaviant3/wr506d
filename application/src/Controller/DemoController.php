95ee1dab386ae23d69f1572e1472c3fa26d95584
Github MMI3 <github@mmi-troyes.fr>
2024-09-16 06:26:31 +0000

    feat api-plateforme

--- application/src/Controller/DemoController.php
+++ application/src/Controller/DemoController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+
 use App\Service\Slugify;
 
 class DemoController extends AbstractController
@@ -12,13 +13,13 @@ class DemoController extends AbstractController
     #[Route('/demo', name: 'app_demo')]
     public function index(Slugify $slugify): Response
     {
-        $phrase = 'éoh salamé, ça farte ?';
-        $slug = $slugify->slugify($phrase);
+        $phrase = 'Test de la phrase numÃ©ro une ?';
+        $demo = $slugify->slugify($phrase);
 
         return $this->render('demo/index.html.twig', [
             'controller_name' => 'DemoController',
             'date' => new \DateTime(),
-            'slug' => $slug, // On passe le slug à la vue
+            'slug' => $demo,
         ]);
     }
 }
