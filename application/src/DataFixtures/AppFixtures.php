d744ee357f2b9bf6bff47fa369c1bbd426428c59
Github MMI3 <github@mmi-troyes.fr>
2024-09-30 16:23:39 +0000

    fix fixture and add category to fixtures.php

--- application/src/DataFixtures/AppFixtures.php
+++ application/src/DataFixtures/AppFixtures.php
@@ -2,62 +2,83 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Faker;
+use Faker\Factory;
 use DateTimeImmutable;
 use App\Entity\Actor;
 use App\Entity\Movie;
+
 class AppFixtures extends Fixture
 {
-    private const TOTAL_ACTORS = 100;
-    private const TOTAL_MOVIES = 100;
-    private const ITEMS_PER_PAGE = 10;
+private const TOTAL_ACTORS = 100;
+private const TOTAL_MOVIES = 100;
+private const TOTAL_CATEGORIES = 10;
+private const ITEMS_PER_PAGE = 10;
 
-    public function load(ObjectManager $manager)
-    {
-        $faker = \Faker\Factory::create();
-        $faker->addProvider(new \Xylis\FakerCinema\Provider\Person($faker));
+public function load(ObjectManager $manager)
+{
+$faker = Factory::create();
+$faker->addProvider(new \Xylis\FakerCinema\Provider\Person($faker));
 
-        for ($i = 0; $i < self::TOTAL_ACTORS; $i++) {
-            $actor = new Actor();
-            $actor->setLastname($faker->actor());
-            $actor->setDob($faker->dateTimeThisCentury());
-            $actor->setBio($faker->text(100)); // Augmenter la longueur du texte pour plus de réalisme
-            $actor->setNationality($faker->country());
-            $actor->setMedia($faker->imageUrl());
-            $actor->setGender($faker->randomElement(['male', 'female']));
-            $actor->setCreatedAt(new DateTimeImmutable());
-            $actor->setUpdatedAt(new DateTimeImmutable());
+// Create Actors
+for ($i = 0; $i < self::TOTAL_ACTORS; $i++) {
+$actor = new Actor();
+$actor->setLastname($faker->actor());
+$actor->setDob($faker->dateTimeThisCentury());
+$actor->setBio($faker->text(100));
+$actor->setNationality($faker->country());
+$actor->setMedia($faker->imageUrl());
+$actor->setGender($faker->randomElement(['male', 'female']));
+$actor->setCreatedAt(new DateTimeImmutable());
+$actor->setUpdatedAt(new DateTimeImmutable());
 
-            $manager->persist($actor);
+$manager->persist($actor);
+}
+
+// Create Movies
+$faker->addProvider(new \Xylis\FakerCinema\Provider\Movie($faker));
+for ($i = 0; $i < self::TOTAL_MOVIES; $i++) {
+$movie = new Movie();
+$movie->setTitle($faker->movie);
+$movie->setDescription($faker->overview);
+$movie->setReleaseDate($faker->dateTimeBetween('-30 years', 'now'));
+$movie->setDuration($faker->numberBetween(60, 180));
+$movie->setEntries($faker->numberBetween(1000, 100000));
+$movie->setDirector($faker->name);
+$movie->setRating($faker->randomFloat(1, 1, 10));
+$movie->setMedia('https://image.tmdb.org/t/p/original//aWeKITRFbbwY8txG5uCj4rMCfSP.jpg');
+$movie->setCreatedAt(new DateTimeImmutable());
+
+$actors = $manager->getRepository(Actor::class)->findAll();
+shuffle($actors);
+$selectedActors = array_slice($actors, 0, 4);
+foreach ($selectedActors as $actor) {
+$movie->addActor($actor);
+}
+
+$manager->persist($movie);
+}
+
+// Create Categories
+    $faker->addProvider(new \Xylis\FakerCinema\Provider\Movie($faker));
+    for ($i = 0; $i < self::TOTAL_CATEGORIES; $i++) {
+        $category = new Category();
+        $category->setTitle($faker->movieGenre());
+        $category->setCreatedAt(new DateTimeImmutable());
+        $category->setUpdatedAt(new DateTimeImmutable());
+
+        $actors = $manager->getRepository(Actor::class)->findAll();
+        shuffle($actors);
+        $selectedActors = array_slice($actors, 0, 4);
+        foreach ($selectedActors as $actor) {
+            $category->addActor($actor);
         }
 
-        $faker = \Faker\Factory::create();
-        $faker->addProvider(new \Xylis\FakerCinema\Provider\Movie($faker));
-
-        for ($i = 0; $i < self::TOTAL_MOVIES; $i++) {
-            $movie = new Movie();
-            $movie->setTitle($faker->movie);
-            $movie->setDescription($faker->overview);
-            $movie->setReleaseDate($faker->dateTimeBetween('-30 years', 'now'));
-            $movie->setDuration($faker->numberBetween(60, 180));
-            $movie->setEntries($faker->numberBetween(1000, 100000));
-            $movie->setDirector($faker->name);
-            $movie->setRating($faker->randomFloat(1, 1, 10));
-            $movie->setMedia('https://image.tmdb.org/t/p/original//aWeKITRFbbwY8txG5uCj4rMCfSP.jpg');
-            $movie->setCreatedAt(new DateTimeImmutable());
-
-            $actors = $manager->getRepository(Actor::class)->findAll();
-            shuffle($actors);
-            $selectedActors = array_slice($actors, 0, 4);
-            foreach ($selectedActors as $actor) {
-                $movie->addActor($actor);
-            }
-
-            $manager->persist($movie);
-        }
-
-        $manager->flush();
+        $manager->persist($category);
     }
+
+$manager->flush();
+}
 }
