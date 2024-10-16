2f6c8c61a5b20bb122cdef410f022bbfde02d884
Github MMI3 <github@mmi-troyes.fr>
2024-10-16 06:14:33 +0000

    fix actor bdd

--- application/src/State/UserPasswordHasher.php
+++ application/src/State/UserPasswordHasher.php
@@ -10,21 +10,17 @@ final class UserPasswordHasher implements ProcessorInterface
     public function __construct(private readonly ProcessorInterface $processor, private readonly UserPasswordHasherInterface $passwordHasher)
     {
     }
-
     public function process($data, Operation $operation, array $uriVariables = [], array $context = [])
     {
         if (!$data->getPlainPassword()) {
             return $this->processor->process($data, $operation, $uriVariables, $context);
         }
-
         $hashedPassword = $this->passwordHasher->hashPassword(
             $data,
             $data->getPlainPassword()
         );
-
         $data->setPassword($hashedPassword);
-        $data->setPlainPassword(null);
-
+        $data->eraseCredentials();
         return $this->processor->process($data, $operation, $uriVariables, $context);
     }
 }
